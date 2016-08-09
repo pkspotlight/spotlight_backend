4ed9cf6b28740209e0a684d07ed80afb25ed3b46
ll <ll@home.lozovski.com>
2016-08-08 20:58:52 -0400

    Fix bucket mismatch

--- config/initializers/aws_client_initialize.rb
+++ config/initializers/aws_client_initialize.rb
@@ -12,4 +12,4 @@ $aws = Aws.config.update({region: 'us-east-1',
                           credentials: Aws::Credentials.new(ENV['aws_access_key_id'],
                                                             ENV['aws_secret_access_key'])})
 $s3 = Aws::S3::Resource.new(region: 'us-east-1')
-$s3_bucket = $s3.bucket('spotlight-temp-bucket')
+$s3_bucket = $s3.bucket(ENV['aws_s3_bucket_name'])
